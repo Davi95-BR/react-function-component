075d2c511573fe43f9464cd785e2d46a76492ced
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-05-08 09:08:56 -0300

    Assincronização e useState da variável nome e sobrenome do formulário

--- src/componente/FormularioCadastro/FormularioCadastro.jsx
+++ src/componente/FormularioCadastro/FormularioCadastro.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
 
 
+
 function FormularioCadastro() {
+    const [nome, setNome] = useState();
+    const { sobrenome, setSobrenome } = useState("");
+
     return (
-        <form>
-            <TextField variant="outlined" id="Nome" label="Nome" fullWidth margin="normal" required />
-            <TextField variant="outlined" id="Sobrenome" label="Sobrenome" fullWidth margin="normal" required />
-            <TextField variant="outlined" id="CPF" label="CPF" fullWidth margin="normal" required />
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                console.log({ nome, sobrenome });
+            }}
+        >
+            <TextField
+                value={nome}
+                onChange={(event) => {
+                    let tmpNome = event.target.value;
+                    if (tmpNome.length >= 3) {
+                        tmpNome = tmpNome(nome.substr(0, 3));
+                    }
+                    setNome(tmpNome);
+                }}
+                variant="outlined" id="nome" label="nome" fullWidth margin="normal" required />
+            <TextField
+                value={sobrenome}
+                onChange={(event) => {
+                    setSobrenome(event.target.value);
+                }}
+                variant="outlined" id="sobrenome" label="sobrenome" fullWidth margin="normal" required />
+            <TextField variant="outlined" id="cpf" label="CPF" fullWidth margin="normal" required />
 
             <FormControlLabel
-                label="Novidades"
-                control={<Switch color="primary" name={'Novidades'} disableRipple required />}
+                label="novidades"
+                control={<Switch color="primary" name={'novidades'} disableRipple />}
             />
             <FormControlLabel
-                label="Promoções"
-                control={<Switch color="primary" name={'Promoções'} disableRipple required />}
+                label="promoções"
+                control={<Switch color="primary" name={'promoções'} disableRipple />}
             />
             <Button type="submit" variant="contained">Cadastrar</Button>
         </form>
