f710e5c92c9f9b360dcacb39417dd30edf88f3d1
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-05-10 06:42:11 -0300

    Adicionando Layout Box no formulário e estilizando, colocando Typography como subtítulo

--- src/componente/FormularioCadastro/FormularioCadastro.jsx
+++ src/componente/FormularioCadastro/FormularioCadastro.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Typography  from '@material-ui/core/Typography'
+
+
 
 
 function FormularioCadastro({ aoEnviar, validarCPF, validarSenha }) {     // Hooks
@@ -14,83 +18,93 @@ function FormularioCadastro({ aoEnviar, validarCPF, validarSenha }) {     // Hoo
 
 
     return (
-        <form
-            onSubmit={(event) => {
-                event.preventDefault();
-                let dados = [nome, sobrenome, cpf, senha, novidades, promocoes]
-                aoEnviar(dados);
-            }}
-        >
-            <TextField
-                value={nome}
-                onChange={(event) => {
-                    setNome(event.target.value);
-                }}
-                variant="outlined" id="nome" label="nome" fullWidth margin="normal" required />
-            <TextField
-                value={sobrenome}
-                onChange={(event) => {
-                    setSobrenome(event.target.value);
-                }}
-                variant="outlined" id="sobrenome" label="sobrenome" fullWidth margin="normal" required />
-            <TextField
-                value={cpf}
-                onChange={(event) => {
-                    setCpf(event.target.value);
-                }}
-                onBlur={(event) => {
-                    const ehValido = validarCPF(cpf);
-                    setErrosCpf({ cpf: ehValido })
-                }}
-                error={!errosCpf.cpf.valido}
-                helperText={errosCpf.cpf.texto}
-                id="CPF"
-                label="CPF"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                required
-            />
-            <TextField
-                value={senha}
-                onChange={(event) => {
-                    setSenha(event.target.value);
-                }}
-                onBlur={(event) => {
-                    const ehValido = validarSenha(senha);
-                    setErrosSenha({ senha:ehValido })
-                }}
-                error={!errosSenha.senha.valido}
-                helperText={errosSenha.senha.texto}
-                id="outlined-basic"
-                label="senha"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                type="password"
-                required
-            />
+
+        <Box
+            border={1}
+            borderColor="primary.main"
+            borderRadius={16}
+            p={2} pt={2} pb={2} mt={2.5}
             
-            <FormControlLabel
-                label="novidades"
-                control={<Switch
-                    checked={novidades}
+            >
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    let dados = [nome, sobrenome, cpf, senha, novidades, promocoes]
+                    aoEnviar(dados);
+                }}
+            >
+                <Typography align="left" variant="h5" component="h1">Informações básicas</Typography>
+                <TextField
+                    value={nome}
                     onChange={(event) => {
-                        setNovidades(event.target.checked);
+                        setNome(event.target.value);
                     }}
-                    color="primary" name={'novidades'} />}
-            />
-            <FormControlLabel
-                label="promoções"
-                control={<Switch
-                    checked={promocoes}
+                    variant="outlined" id="nome" label="nome" fullWidth margin="normal" required />
+                <TextField
+                    value={sobrenome}
                     onChange={(event) => {
-                        setPromocoes(event.target.checked);
+                        setSobrenome(event.target.value);
                     }}
-                    color="primary" name={'promocoes'} />}
-            />
-            <Button type="submit" variant="contained">Cadastrar</Button>
-        </form>
+                    variant="outlined" id="sobrenome" label="sobrenome" fullWidth margin="normal" required />
+                <TextField
+                    value={cpf}
+                    onChange={(event) => {
+                        setCpf(event.target.value);
+                    }}
+                    onBlur={(event) => {
+                        const ehValido = validarCPF(cpf);
+                        setErrosCpf({ cpf: ehValido })
+                    }}
+                    error={!errosCpf.cpf.valido}
+                    helperText={errosCpf.cpf.texto}
+                    id="CPF"
+                    label="CPF"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    required
+                />
+                <TextField
+                    value={senha}
+                    onChange={(event) => {
+                        setSenha(event.target.value);
+                    }}
+                    onBlur={(event) => {
+                        const ehValido = validarSenha(senha);
+                        setErrosSenha({ senha: ehValido })
+                    }}
+                    error={!errosSenha.senha.valido}
+                    helperText={errosSenha.senha.texto}
+                    id="outlined-basic"
+                    label="senha"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    type="password"
+                    required
+                />
+
+                <FormControlLabel
+                    label="novidades"
+                    control={<Switch
+                        checked={novidades}
+                        onChange={(event) => {
+                            setNovidades(event.target.checked);
+                        }}
+                        color="primary" name={'novidades'} />}
+                />
+                <FormControlLabel
+                    label="promoções"
+                    control={<Switch
+                        checked={promocoes}
+                        onChange={(event) => {
+                            setPromocoes(event.target.checked);
+                        }}
+                        color="primary" name={'promocoes'} />}
+                />
+                <Button type="submit" variant="contained">Cadastrar</Button>
+            </form>
+        </Box>
     );
 }
 
